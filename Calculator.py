7000468ce2277686fd497880ce3bcb47229ab206
avi <kordeavinash86@gmail.com>
2021-04-10 08:40:34 +0530

    add file

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,29 @@
+   
+def add(n1,n2):
+    n3=n1+n2
+    return (n3)
+def sub(n1,n2):
+    n3=n1-n2
+    return (n3)
+def mul (n1,n2):
+    n3=n1*n2
+    return (n3)
+def div(n1,n2):
+    n3=n1/n2
+    return (n3)
+choice=input("which oprator you want? choice:\n1 add\n2 sub\n3 mul\n4 div\n")
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+if (choice == 1):
+    print(n1,"+",n2,add(n1,n2))
+elif (choice == 2):
+    print(n1, "-", n2, sub(n1,n2))
+elif (choice == 3):
+    print(n1, "*", n2, mul(n1,n2))
+else:
+    print(n1, "/", n2, div(n1,n2))
 
-#### This Function Performs Addition
 
-#### This Function Performs Subtraction
-
-#### This Function Performs Multiplication
-
-#### This Function Performs Division
-
-#### This Function Shows Modulus
+    
 
 
-## Print Statements and INPUT
-
-
-## LOGIC FOR CALCULATION BELOW
